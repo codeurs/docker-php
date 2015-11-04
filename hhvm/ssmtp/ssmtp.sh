2e90226d76be8b3189e08857cf9836756a8683d9
root <root@codeu.rs>
2015-11-04 16:58:45 +0100

    Ssmtp modified

--- hhvm/ssmtp/ssmtp.sh
+++ hhvm/ssmtp/ssmtp.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-sed -i "s/^mailhub=*/mailhub=${SMTP_HOST}/" /etc/ssmtp/ssmtp.conf
-sed -i "s/^rewriteDomain=*/rewriteDomain=${SMTP_DOMAIN}/" /etc/ssmtp/ssmtp.conf
-sed -i "s/^hostname=*/hostname=${SMTP_DOMAIN}/" /etc/ssmtp/ssmtp.conf
-sed -i "s/^UseTLS=*/UseTLS=${SMTP_TLS}/" /etc/ssmtp/ssmtp.conf
-sed -i "s/^UseSTARTTLS=*/UseSTARTTLS=${SMTP_STARTTLS}/" /etc/ssmtp/ssmtp.conf
-sed -i "s/^AuthUser=*/AuthUser=${SMTP_USERNAME}/" /etc/ssmtp/ssmtp.conf
-sed -i "s/^AuthPass=*/AuthPass=${SMTP_PASSWORD}/" /etc/ssmtp/ssmtp.conf
+echo "root=info
+mailhub=${SMTP_HOST}
+rewriteDomain=${SMTP_DOMAIN}
+hostname=${SMTP_DOMAIN}
+UseTLS=${SMTP_TLS}
+UseSTARTTLS=${SMTP_STARTTLS}
+AuthUser=${SMTP_USERNAME}
+AuthPass=${SMTP_PASSWORD}
+FromLineOverride=YES" > /etc/ssmtp/ssmtp.conf
 
 exit 0
